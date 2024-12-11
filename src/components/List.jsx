aa64980d1514b76ed8eecdbe8772673da02e4d71
p3drac0 <75147146+p3drac0@users.noreply.github.com>
2024-12-11 20:43:07 +0000

    First commit

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,54 +1,92 @@
 
 
 import { useState } from 'react';
+import { useEffect } from 'react';
 
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Button from 'react-bootstrap/Button';
+import { type } from '@testing-library/user-event/dist/type';
 
 
 
 const ListComponent = () => {
 
+
   const [list, setList] = useState([])
+  const [text, setText] = useState("")
 
-  const inputPlaceHolder = 'Tip something to do'
+  // Definition of the function for add item to list
   function addItemToList(text) {
-    const tarea = {
-      id: crypto.randomUUID(),
-      task: text
-    }
 
-    setList([...list, tarea])
+    const task = {
+      label: text,
+      is_done: false
+    }
+    fetch('https://playground.4geeks.com/todo/todos/Pedro', { method: "POST", body: JSON.stringify(task), headers: { "Content-Type": "application/json" } })
+      .then(() => refreshList())
+
   }
 
+  //Definition of the function for refreshing my list
 
-  return (<>
+  function refreshList() {
+    fetch('https://playground.4geeks.com/todo/users/Pedro', { method: "GET" })
+      .then((resp) => {
 
-    <InputGroup className="mb-3">
-      <Form.Control onKeyDown={(e) => {
-        if (e.key === "Enter") {
-          addItemToList(e.target.value);
-          e.target.value = "";
 
-      }}}
-        placeholder={inputPlaceHolder}
-        aria-label="Default"
-        aria-describedby="inputGroup-sizing-default"
-      />
-    </InputGroup>
-    {list.map((element) => {
-      return (
-        <div style={{width:"100% " , display:"flex" , justifyContent:"space-between"}} id={element.id}>{element.task}
-        
-        <Button variant="outline-secondary">X</Button>
-        </div>
-      )
+        return resp.json()
+      })
+      .then((data) => {
+        setList(data.todos)
+      })
+      .catch((error) => { console.log("La cagaste") })}
 
-    })}
 
-  </>
-  )
-}
 
-export default ListComponent
+    //Definition of the function for deleting my list
+
+    function deleteItem(task_id) {
+      fetch(`https://playground.4geeks.com/todo/todos/${task_id}`, { method: "DELETE" })
+        .then(() => refreshList())
+
+    }
+    //First call for refreshing the list
+    useEffect(() => {
+      refreshList()
+    }, []);
+
+
+
+
+
+    return (<>
+
+      <InputGroup className="mb-3">
+        <Form.Control value={text} onChange={(e) => { setText(e.target.value) }} onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            addItemToList(text);
+            setText("");
+
+          }
+        }}
+          placeholder={'Tip something to do'}
+          aria-label="Default"
+          aria-describedby="inputGroup-sizing-default"
+        />
+      </InputGroup>
+      {list.map((element) => {
+        return (
+          <div style={{ width: "100% ", display: "flex", justifyContent: "space-between" }} id={element.id}>{element.label}
+
+            <Button variant="outline-secondary" onClick={() => deleteItem(element.id)}> X </Button>
+          </div>
+        )
+
+      })}
+
+    </>
+    )
+  }
+
+  export default ListComponent
